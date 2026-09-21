927134d6ef6cdaf1770a9acfa2176773e3b4a9c9
sabrinakayla <sabrinakuliah10@gmail.com>
2026-09-21 11:10:11 +0700

    first commit

---
+++ praktikum.py
@@ -0,0 +1,11 @@
+nilai =(input("Masukkan Nilai : "))
+if nilai >= 90 :
+    print("Excellent performance")
+elif nilai >= 80 :
+    print("Very Good performance")
+elif nilai >= 70 :
+    print("Good performance")
+elif nilai >= 60 :
+    print("average performance")
+else :
+    print("Silahkan Belajar Lagi")
