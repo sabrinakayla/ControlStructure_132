c95fb48450f80a5bfa608ae24aae6f9755cb959a
sabrinakayla <sabrinakuliah10@gmail.com>
2026-09-21 11:15:46 +0700

    sintaks jawaban nomor 2

--- praktikum.py
+++ praktikum.py
@@ -9,3 +9,16 @@ elif nilai >= 60 :
     print("average performance")
 else :
     print("Silahkan Belajar Lagi")
+
+a = float(input("Masukkan angka pertama: "))
+b = float(input("Masukkan angka kedua: "))
+c = float(input("Masukkan angka ketiga: "))
+
+if a >= b and a >= c:
+    paling_besar = a
+elif b >= a and b >= c:
+    paling_besar = b
+else:
+    paling_besar = c
+
+print("Angka terbesar adalah:", paling_besar)
